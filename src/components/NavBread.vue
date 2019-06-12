5ec057d9b8a9a80f3875af5ba12de5c3b423dcd4
worldsong <772083612@qq.com>
2019-06-12 22:01:57 +0800

    GoodsList组件拆分。

---
+++ src/components/NavBread.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="nav-breadcrumb-wrap">
+      <div class="container">
+        <nav class="nav-breadcrumb">
+          <a href="/">主页</a>
+          <span>商品列表</span>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import './../assets/css/nav-bread.css'
+
+  export default {
+    name: 'NavBread',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/NavFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <footer class="footer">
+      <div class="footer__wrap">
+        <div class="footer__secondary">
+          <div class="footer__inner">
+            <div class="footer__secondary__nav">
+              <span>Copyright©2013-2017 songfens.club All Rights Reserved.</span>
+              <a href="http://songfens.club/">
+                关于我们
+              </a>
+              <a href="http://songfens.club/">
+                新前端|Song
+              </a>
+              <a href="http://songfens.club/">
+                版权所有 2008-2020 湘ICP备18003879号-1
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+  import './../assets/css/nav-footer.css'
+
+  export default {
+    name: 'NavFooter',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
