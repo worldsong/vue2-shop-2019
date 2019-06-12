0c14da8605bb1019aed1bfd931f0fbd722d7dd8f
worldsong <772083612@qq.com>
2019-06-12 21:38:48 +0800

    创建views目录来放置页面。

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GoodsList',
+    data () {
+      return {
+        msg: '第一个页面：商品列表页'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
