9b04287cf5a0347a243e00f23358264b6f3b9272
worldsong <772083612@qq.com>
2019-06-12 22:05:08 +0800

    样式放入组件内。

--- src/components/NavBread.vue
+++ src/components/NavBread.vue
@@ -12,8 +12,6 @@
 </template>
 
 <script>
-  import './../assets/css/nav-bread.css'
-
   export default {
     name: 'NavBread',
     data () {
@@ -26,5 +24,45 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  /** the page max width **/
+  .container {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0 10px;
+  }
+
+  .nav-breadcrumb-wrap {
+    background: #f0f0f0;
+  }
+
+
+  /** 面包屑 **/
+  .nav-breadcrumb {
+    padding: 10px 0;
+    line-height: 25px;
+    font-size: 14px;
+  }
+
+  .nav-breadcrumb a {
+    position: relative;
+    margin-right: 16px;
+    color: #999;
+  }
+
+  .nav-breadcrumb a:after {
+    position: absolute;
+    top: 3px;
+    right: -12px;
+    content: "/";
+    line-height: 1.2;
+  }
+
+  .nav-breadcrumb a:hover {
+    color: #ee7a23;
+  }
+
+  .nav-breadcrumb span {
+    color: #ee7a23;
+  }
 
 </style>
