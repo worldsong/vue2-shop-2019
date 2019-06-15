a45266c92792185458e3fd2735ca255078fcd95b
worldsong <772083612@qq.com>
2019-06-15 20:57:09 +0800

    引入axios,前后端交互实现商品列表页。

--- config/index.js
+++ config/index.js
@@ -10,7 +10,11 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/goods': {// 8080/goods
+        target: 'http://localhost:4000'
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -20,7 +24,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
