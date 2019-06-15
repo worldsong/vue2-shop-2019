b9a32451b84174e02a62607ebb2b82962b8a2dc1
worldsong <772083612@qq.com>
2019-06-15 21:53:22 +0800

    使用vue-lazyload做图片懒加载。

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
+Vue.use(VueLazyLoad, {
+  loading: 'static/loading-svg/loading-bars.svg',
+  attempt: 3, // default 1
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
