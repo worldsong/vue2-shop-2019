4de13d281dcfd9d3af5570bde47924af6e080de1
worldsong <772083612@qq.com>
2019-06-22 21:57:00 +0800

    面包屑导航slot。

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <nav-header></nav-header>
-    <nav-bread></nav-bread>
+    <nav-bread>
+      <span>购物车</span>
+    </nav-bread>
     <svg style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1"
          xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <defs>
