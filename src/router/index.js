c3cb45ed8931e8459b6c99f0fc7339fb782fa283
worldsong <772083612@qq.com>
2019-06-22 21:38:27 +0800

    购物车页面拆分 --》 Vue组件化。

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList.vue'
+import Cart from '@/views/Cart.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'GoodsList',
       component: GoodsList
-    }
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
   ]
 })
