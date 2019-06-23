42505f9fe6f2acf22b11be320fc46533ae206b8d
worldsong <772083612@qq.com>
2019-06-23 21:40:20 +0800

    收货地址页--》Vue组件化。

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList.vue'
 import Cart from '@/views/Cart.vue'
+import Address from '@/views/Address.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/cart',
       name: 'Cart',
       component: Cart
+    },
+    {
+      path: '/address',
+      name: 'Address',
+      component: Address
     },
   ]
 })
