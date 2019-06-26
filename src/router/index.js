2f51fe968919fd3edd759b01cfd2d642f3845bd2
worldsong <772083612@qq.com>
2019-06-26 21:24:13 +0800

    订单确认页--》Vue组件化。

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList.vue'
 import Cart from '@/views/Cart.vue'
 import Address from '@/views/Address.vue'
+import OrderConfirm from '@/views/OrderConfirm.vue'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/address',
       name: 'Address',
       component: Address
+    },
+    {
+      path: '/OrderConfirm',
+      name: 'OrderConfirm',
+      component: OrderConfirm
     },
   ]
 })
